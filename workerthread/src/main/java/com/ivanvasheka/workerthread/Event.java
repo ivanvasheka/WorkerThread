aa29cb9bf00022ccb5bfb67b62a46e7e644bbd14
ivanvasheka <ivanvasheka@gmail.com>
2016-08-04 16:35:12 +0300

    improves event class by adding useful methods

--- workerthread/src/main/java/com/ivanvasheka/workerthread/Event.java
+++ workerthread/src/main/java/com/ivanvasheka/workerthread/Event.java
@@ -30,7 +30,7 @@ public class Event<T> {
      */
     public static final int TYPE_ONE_SHOT = 2;
 
-    @IntDef({TYPE_NONE, TYPE_LATEST_ONLY})
+    @IntDef({TYPE_NONE, TYPE_LATEST_ONLY, TYPE_ONE_SHOT})
     @Retention(RetentionPolicy.SOURCE)
     public @interface Type {
     }
@@ -57,109 +57,127 @@ public class Event<T> {
         this.subscriber = subscriber;
     }
 
-    @Nullable
-    public Number getNumber() {
-        return number;
-    }
-
-    @Nullable
-    public String getMessage() {
-        return message;
-    }
-
-    @Nullable
-    public Bundle getExtra() {
-        return extra;
-    }
-
-    @Nullable
-    public T getData() {
-        return data;
-    }
+    //region Subscriber methods
 
     @Nullable
     public Class<?> getSubscriber() {
         return subscriber;
     }
 
-    public void post() {
-        WorkerThread.get().post(this);
+    public void setSubscriber(@NonNull Class<?> subscriber) {
+        this.subscriber = subscriber;
     }
+
+    //endregion
+
+    //region Type methods
 
     @Type
     public int getType() {
         return type;
     }
 
-    public boolean useMainThread() {
-        return useMainThread;
+    public void setType(@Type int type) {
+        this.type = type;
+    }
+
+    //endregion
+
+    //region Number methods
+
+    @Nullable
+    public Number getNumber() {
+        return number;
     }
 
     public void setNumber(int number) {
         this.number = number;
     }
 
+    /**
+     * Returns int value if the event was supplied with int as a number.
+     *
+     * @return <b>int</b> value of events number, or <b>0</b>.
+     */
+    public int getInt() {
+        return getInt(0);
+    }
+
+    /**
+     * Returns int value if the event was supplied with int as a number.
+     *
+     * @param defValue to return if number is missing.
+     * @return <b>int</b> value of events number, or <b>defValue</b>.
+     */
+    public int getInt(int defValue) {
+        if (number == null) {
+            return defValue;
+        } else {
+            return number.intValue();
+        }
+    }
+
+    //endregion
+
+    //region Message methods
+
+    @Nullable
+    public String getMessage() {
+        return message;
+    }
+
     public void setMessage(@NonNull String message) {
         this.message = message;
+    }
+
+    /**
+     * @return <b>true</b> if message not null, <b>false</b> otherwise
+     */
+    public boolean hasMessage() {
+        return message != null;
+    }
+
+    //endregion
+
+    //region Bundle methods
+
+    @Nullable
+    public Bundle getExtra() {
+        return extra;
     }
 
     public void setExtra(@NonNull Bundle extra) {
         this.extra = extra;
     }
 
+    public boolean hasExtra(String key) {
+        return extra != null && extra.containsKey(key);
+    }
+
+    //endregion
+
+    //region Data methods
+
+    @Nullable
+    public T getData() {
+        return data;
+    }
+
     public void setData(@NonNull T data) {
         this.data = data;
     }
 
-    public void setSubscriber(@NonNull Class<?> subscriber) {
-        this.subscriber = subscriber;
+    //endregion
+
+    boolean useMainThread() {
+        return useMainThread;
     }
 
-    public void setType(@Type int type) {
-        this.type = type;
+    public void post() {
+        WorkerThread.get().post(this);
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Event event = (Event) o;
-
-        if (type != event.type) return false;
-        if (subscriber != null ? !subscriber.equals(event.subscriber) : event.subscriber != null)
-            return false;
-        if (message != null ? !message.equals(event.message) : event.message != null)
-            return false;
-        if (number != null ? !number.equals(event.number) : event.number != null) return false;
-        //noinspection SimplifiableIfStatement
-        if (extra != null ? !extra.equals(event.extra) : event.extra != null) return false;
-        return !(data != null ? !data.equals(event.data) : event.data != null);
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = subscriber != null ? subscriber.hashCode() : 0;
-        result = 31 * result + type;
-        result = 31 * result + (message != null ? message.hashCode() : 0);
-        result = 31 * result + (number != null ? number.hashCode() : 0);
-        result = 31 * result + (extra != null ? extra.hashCode() : 0);
-        result = 31 * result + (data != null ? data.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Event{" +
-                "subscriber='" + subscriber + '\'' +
-                ", type=" + type +
-                ", number=" + number +
-                ", message='" + message + '\'' +
-                ", extra=" + extra +
-                ", data=" + data +
-                '}';
-    }
+    //region Builder pattern realisation
 
     public static <T> Builder<T> toEveryone() {
         return new Builder<>();
@@ -179,6 +197,16 @@ public class Event<T> {
 
         public Builder<T> withType(@Type int type) {
             event.type = type;
+            return this;
+        }
+
+        public Builder<T> latestOnly() {
+            event.type = TYPE_LATEST_ONLY;
+            return this;
+        }
+
+        public Builder<T> oneShot() {
+            event.type = TYPE_ONE_SHOT;
             return this;
         }
 
@@ -220,5 +248,49 @@ public class Event<T> {
         public void post() {
             event.post();
         }
+    }
+
+    //endregion
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Event event = (Event) o;
+
+        if (type != event.type) return false;
+        if (subscriber != null ? !subscriber.equals(event.subscriber) : event.subscriber != null)
+            return false;
+        if (message != null ? !message.equals(event.message) : event.message != null)
+            return false;
+        if (number != null ? !number.equals(event.number) : event.number != null) return false;
+        //noinspection SimplifiableIfStatement
+        if (extra != null ? !extra.equals(event.extra) : event.extra != null) return false;
+        return !(data != null ? !data.equals(event.data) : event.data != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = subscriber != null ? subscriber.hashCode() : 0;
+        result = 31 * result + type;
+        result = 31 * result + (message != null ? message.hashCode() : 0);
+        result = 31 * result + (number != null ? number.hashCode() : 0);
+        result = 31 * result + (extra != null ? extra.hashCode() : 0);
+        result = 31 * result + (data != null ? data.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "subscriber='" + subscriber + '\'' +
+                ", type=" + type +
+                ", number=" + number +
+                ", message='" + message + '\'' +
+                ", extra=" + extra +
+                ", data=" + data +
+                '}';
     }
 }
